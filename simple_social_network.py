6fba73ead3cd9130f58a7dcfd9dac6484839b42b
M. Ali <aliusacomm@gmail.com>
2022-08-18 03:29:26 -0400

    added simple_social_netwrok.py

---
+++ simple_social_network.py
@@ -0,0 +1,9 @@
+""" A simple social network for tracking connections between people."""
+
+class Person:
+    """ A person in the social network.
+
+    Attributes:
+        name (str): the person's name.
+        connection (set of Persons): other people in the soical network who know this person
+    """
